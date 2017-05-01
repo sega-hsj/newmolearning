0df6176da107368ec4c4e8e1cdbb0a82951fd6fd
Frank Scarlet <hferfrank@sina.com>
2017-05-01 23:05:26 +0800

    openFileDialog saveFileDialog

--- Project2/MyForm.cpp
+++ Project2/MyForm.cpp
@@ -1,10 +1,11 @@
 #include "MyForm.h"
 using namespace Project2;
+using namespace System ::IO;
+[STAThread]
 int main(array<System::String ^>^args)
 {
 	Application::EnableVisualStyles();
 	Application::Run(gcnew MyForm());
-
 	return 0;
 }
 
--- Project2/MyForm.h
+++ Project2/MyForm.h
@@ -8,6 +8,7 @@ namespace Project2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
 
 	/// <summary>
 	/// MyForm 摘要
@@ -40,6 +41,11 @@ namespace Project2 {
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::NotifyIcon^  notifyIcon1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	private: System::Windows::Forms::SaveFileDialog^  saveFileDialog1;
+	private: System::Windows::Forms::Button^  button3;
 	private: System::ComponentModel::IContainer^  components;
 
 	private:
@@ -62,6 +68,11 @@ namespace Project2 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->notifyIcon1 = (gcnew System::Windows::Forms::NotifyIcon(this->components));
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -106,11 +117,51 @@ namespace Project2 {
 			this->notifyIcon1->Text = L"notifyIcon1";
 			this->notifyIcon1->Visible = true;
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(755, 151);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(175, 67);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(778, 267);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(255, 28);
+			this->textBox2->TabIndex = 5;
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			this->openFileDialog1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::openFileDialog1_FileOk);
+			// 
+			// saveFileDialog1
+			// 
+			this->saveFileDialog1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::saveFileDialog1_FileOk);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(740, 54);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(190, 91);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1152, 498);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->checkBox1);
@@ -128,6 +179,8 @@ namespace Project2 {
 		label1->Text = "哈哈哈";
 		textBox1->Text = "好玩吗";
 		checkBox1->Text = "退出";
+		button2->Text = "保存";
+		button3->Text = "打开";
 		
 	}
 private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) 
@@ -135,5 +188,41 @@ private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::
 
 	Close();
 }
+
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
+{
+	saveFileDialog1->InitialDirectory = "C:\\Users\flg\Documents\\";
+	saveFileDialog1->Filter = "文本文件(*.txt)|*.txt|全部文件(*.*)|*.*";
+	saveFileDialog1->FilterIndex = 1;
+	saveFileDialog1->RestoreDirectory = true;
+	saveFileDialog1->CreatePrompt = true;
+	saveFileDialog1->ShowDialog();
+}
+
+private: System::Void openFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) 
+{
+	String^ path = openFileDialog1->FileName;
+	StreamReader^ sr = File::OpenText(path);
+	textBox2->Text = sr->ReadToEnd();
+	sr->Close();
+}
+
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) 
+{
+	textBox2->Text = "";
+	openFileDialog1->InitialDirectory = "C:\\Users\flg\Documents\\";
+	openFileDialog1->FilterIndex = 1;
+	openFileDialog1->RestoreDirectory = true;
+	openFileDialog1->ShowDialog();
+}
+
+private: System::Void saveFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) 
+{
+	String^ path = saveFileDialog1->FileName;
+	StreamWriter^ sw = File::CreateText(path);
+	sw->Write(textBox2->Text);
+	sw->Close();
+}
+
 };
 }
